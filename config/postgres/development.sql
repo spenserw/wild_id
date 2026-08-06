d389b5e30b8ad57e0d1211321f36ec55f21e03ce
Spenser Williams <spenser@w2cs.com>
2026-08-05 20:02:22 -0400

    dev config

--- config/postgres/development.sql
+++ config/postgres/development.sql
@@ -1,3 +1,4 @@
 -- Extra databases for local development / test.
 -- Runs once on first Postgres container init (empty data volume).
 CREATE DATABASE wild_id_test OWNER wild_id;
+CREATE DATABASE wild_id_development_queue OWNER wild_id;
