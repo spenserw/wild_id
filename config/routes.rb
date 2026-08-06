e5d9a6f230087dca38de393a2c7035e74ac1b980
Spenser Williams <spenser@w2cs.com>
2026-08-05 20:03:49 -0400

    solid queue config

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  mount SolidQueueDashboard::Engine, at: "/solid-queue"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
